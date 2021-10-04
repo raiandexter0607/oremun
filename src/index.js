d2cf289086513354965689a1034848425d7ac60b
bduque <bduque@lean-tech.io>
2021-10-03 19:52:31 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const Li = ({ children, estado }) => {
+  return (
+    <li>
+      {children}
+      {estado}
+    </li>
+  );
+};
+
+const X = () => (
+  <ul>
+    <Li estado={" feliz"} casa={false} edad={24}>
+      Chanchito
+    </Li>
+    <Li estado={" triste"}>Chanchito</Li>
+    <Li estado={" emocionado"}>Chanchito</Li>
+  </ul>
 );
+
+ReactDOM.render(<X />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
